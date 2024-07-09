8eed5abfe4f43f508f9bdca070b26b549853aab6
Zeel Makwana <124759630+zeel1012@users.noreply.github.com>
2024-07-09 18:37:36 +0530

    Gift SKU Validation with Update of Magento 2.4.7 (#3)
    * update readme file

---
+++ Plugin/SalesRule/Model/Rule/ValidateGiftSkus.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageSuite\FreeGift\Plugin\SalesRule\Model\Rule;
+
+class ValidateGiftSkus
+{
+    protected \MageSuite\FreeGift\SalesRule\Validator\GiftSku $giftSkuValidator;
+
+    public function __construct(
+        \MageSuite\FreeGift\SalesRule\Validator\GiftSku $giftSkuValidator
+    ) {
+        $this->giftSkuValidator = $giftSkuValidator;
+    }
+
+    public function aroundValidateData(
+        \Magento\SalesRule\Model\Rule $subject,
+        callable $proceed,
+        \Magento\Framework\DataObject $dataObject
+    ) {
+        $result = $proceed($dataObject);
+        $isSkuValid = $this->giftSkuValidator->isValid($dataObject);
+
+        if ($isSkuValid) {
+            return $result;
+        }
+
+        if ($result === true) {
+            $result = [];
+        }
+
+        $result[] = __('Provided gift SKU does not exist in the catalog.');
+        return $result;
+    }
+}
